0cc85c4823324da2af13e6ab4b411f9e8d01efbc
Edouard Ybañez <145130147+Shifts334@users.noreply.github.com>
2024-09-14 20:20:23 +0800

    Practice Activities
    .

---
+++ stacks/stacksArrays.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAX 10
+
+typedef struct stack{
+    int dataArr[MAX];
+    int top;
+}stack;
+
+void initialize(stack *s);
+void push(stack *s, int data);
+void pop(stack *s);
+int isEmpty(stack s);
+int isFull(stack s);
+void peek(stack s);
+void display(stack s);
+
+void main(){
+    stack test;
+    initialize(&test);
+    push(&test, 10);
+    push(&test, 20);
+    push(&test, 30);
+    display(test);
+    pop(&test);
+    peek(test);
+    push(&test, 40);
+    display(test);
+
+}
+
+void initialize(stack *s){
+    s->top = -1;
+}
+
+void push(stack* s, int elem){
+    if(isFull(*s)){
+        printf("\nStack is full\n");
+    }
+    else{
+        s->dataArr[++s->top] = elem;
+    }
+}
+
+void pop(stack *s){
+    if(isEmpty((*s))){
+        printf("\nStack i1s empty.");
+    }
+    else{
+        printf("\nElement popped: %d", s->dataArr[s->top--]);
+    }
+}
+
+int isEmpty(stack s){
+    return(s.top == -1)?1:0;
+}
+
+int isFull(stack s){
+    return(s.top == MAX-1)?1:0;
+}
+
+void peek(stack s){
+    if(isEmpty(s)){
+        printf("\nStack is empty\n");
+    }
+    else{
+        printf("\nelement peek: %d\n",s.dataArr[s.top]);
+    }
+}
+
+void display(stack s){
+    if(isEmpty(s)){
+        printf("\nStack is empty.");
+    }
+    else{
+        printf("Display: ");
+        for(int i = s.top; i>=0; i--){
+            printf("%d ", s.dataArr[i]);
+        }
+    }
+}
