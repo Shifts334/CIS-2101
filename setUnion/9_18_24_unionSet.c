55c3807f4e0d2aa31ad18d429b5ea2a839ea8a47
Edouard Ybañez <145130147+Shifts334@users.noreply.github.com>
2024-09-18 20:45:44 +0800

    Union Set assignment update

--- setUnion/9_18_24_unionSet.c
+++ setUnion/9_18_24_unionSet.c
@@ -15,15 +15,48 @@ int isempty(Sets *set);
 void display(Sets *set);
 void insertSorted(Sets *set, int num);
 void delete(Sets *set, int num);
-Sets Union(Sets *set1, Sets *set2);
-Sets intersection(Sets *set1, Sets *set2);
-Sets difference(Sets *set1, Sets *set2);
+Sets Union(Sets set1, Sets set2);
+Sets intersection(Sets set1, Sets set2);
+Sets difference(Sets set1, Sets set2);
 
 //main function
 int main()
 {
     Sets set1 = {{1,2,3,4,5,6,7}, 7};
     Sets set2 = {{3,4,5,6,7,8,9}, 7};
+    Sets uniSet = Union(set1, set2);
+    Sets interSet = intersection(set1, set2);
+    Sets diffSet = difference(set1, set2);
+
+    printf("Original Sets:\n");
+    printf("Set1: ");
+    display(&set1);
+    printf("Set2: ");
+    display(&set2);
+
+    insertSorted(&set1, 8);
+    printf("\nSet1 after inserting 8: ");
+    display(&set1);
+    insertSorted(&set2, 4);
+    printf("Set2 after inserting 4: ");
+    display(&set2);
+
+    delete(&set1, 8);
+    printf("\nSet1 after deleting 8: ");
+    display(&set1);
+    delete(&set2, 4);
+    printf("Set2 after deleting 4: ");
+    display(&set2);
+
+    printf("\nUnion of Set1 and Set2: \n");
+    display(&uniSet);
+    printf("\nIntersection of Set1 and Set2: \n");
+    display(&interSet);
+    printf("\nDifference of Set1 and Set2: \n");
+    display(&diffSet);
+
+    return 0;
+
 
 }
 
@@ -43,7 +76,7 @@ int  isfull(Sets *set)
 
 int isempty(Sets *set)
 {
-    if(set->count == 0)
+    if(set->count == -1)
     {
         return 1;
     }
@@ -79,18 +112,20 @@ void display(Sets *set)
 
 void insertSorted(Sets *set , int num)
 {
+    int i;
     if(isfull(set))
     {
         printf("Set is full\n");
     }
     else
-    {
-        for(int i=set->count; i!=0 && num>=set->arr[i]; i--)
+    {   //slight error with the for loop 2nd condition
+        for(i=set->count; i!=0 && num<set->arr[i-1]; i--)
         {
             set->arr[i] = set->arr[i-1];
         }
-        set->arr[set->count] = num;
+        set->arr[i] = num;
         set->count++;
+        // I didn't take into account the case of which index I would be inserting in.
     }
 }
 
@@ -115,42 +150,53 @@ void delete(Sets *set, int num)
     }
 }
 
-Sets Union(Sets *set1, Sets *set2)
+Sets Union(Sets set1, Sets set2)
 {
     Sets set3 = {{}, 0};
-    for(int i=0; i<set1->count; i++)
+    for(int i=0; i<set1.count; i++)
     {
-        insertSorted(&set3, set1->arr[i]);
+        insertSorted(&set3, set1.arr[i]);
     }
-    for(int i=0; i<set2->count; i++)
+    for(int i=0; i<set2.count; i++)
     {
-        insertSorted(&set3, set2->arr[i]);
-    }
-    return set3;
-}
-
-Sets intersection(Sets *set1, Sets *set2)
-{
-    Sets set3 = {{}, 0};
-    for(int i=0; i<set1->count; i++)
-    {
-        if(find(set2, set1->arr[i]) != -1)
+        if(find(&set3, set2.arr[i]) == -1)
         {
-            insertSorted(&set3, set1->arr[i]);
+            insertSorted(&set3, set2.arr[i]);
         }
     }
     return set3;
 }
 
-Sets difference(Sets *set1, Sets *set2)
+Sets intersection(Sets set1, Sets set2)
 {
     Sets set3 = {{}, 0};
-    for(int i=0; i<set1->count; i++)
+    for(int i=0; i<set1.count; i++)
     {
-        if(find(set2, set1->arr[i]) == -1)
+        if(find(&set2, set1.arr[i]) != -1)
         {
-            insertSorted(&set3, set1->arr[i]);
+            insertSorted(&set3, set1.arr[i]);
         }
     }
     return set3;
 }
+
+Sets difference(Sets set1, Sets set2)
+{
+    Sets set3 = {{}, 0};
+    for(int i=0; i<set1.count; i++)
+    {
+        if(find(&set2, set1.arr[i]) == -1)
+        {
+            insertSorted(&set3, set1.arr[i]);
+        }
+    }
+    for(int i=0; i<set2.count; i++)
+    {
+        if(find(&set1, set2.arr[i]) == -1)
+        {
+            insertSorted(&set3, set2.arr[i]);
+        }
+    }
+    return set3;
+}
+
