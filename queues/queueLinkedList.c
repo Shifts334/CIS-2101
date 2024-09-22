143a83715f1ed8ab16dd8b410742b77cac405cca
Edouard Ybañez <145130147+Shifts334@users.noreply.github.com>
2024-09-22 13:37:08 +0800

    updates

--- queues/queueLinkedList.c
+++ queues/queueLinkedList.c
@@ -1,9 +1,54 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct queue{
-    int data;
-    struct queue *next;
-}queue, *queuePtr;
+#define MAX 10
 
-//prototypes 
+typedef struct node{
+    int data;
+    struct node *next;
+    int front;
+    int rear;
+}node;
+
+typedef struct queue{
+	int front;
+	int rear;
+}queue;
+
+//prototypes
+void initialQ(queue *line);
+int isFull(queue *line);
+int isEmpty(queue *line);
+void enqueue(); //Rear
+void dequeue(); //Front
+
+
+//main function
+int main(){
+	queuePtr Qline;
+	
+}
+
+void initialQ(queue *line){
+	line->front = line->rear = -1;
+}
+
+int isFull(queue *line){
+	if((line->rear) == MAX -1){
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
+int isEmpty(queue *line){
+	if((line->front) == -1){
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
+
+
+
